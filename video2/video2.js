a68252ec60dbb79f83a41b7130d25f1ba1685ced
nhochuy519 <huymini519@gmail.com>
2023-01-02 15:51:27 +0700

    chapter 2 video 2

---
+++ video2/video2.js
@@ -0,0 +1,21 @@
+/* 
+
+    Hàm new Date().getDate() đổi thành getYear() để xem năm
+    Dùng để xem hôm nay là ngày bao nhiêu
+*/
+console.log('Hello world form javascript')
+console.log('Hello world with HUY')
+
+/* 
+    Muốn check một biến có giá trị như thế nào thì ta dùng
+        Dùng hàm console.log('thông tin bạn muốn hiển thị')
+        dùng trên trình duyệt
+
+        Khi thực hiện câu lệnh xong thì Cửa sổ tab console sẽ hiện dòng chữ bạn muốn in
+*/
+
+
+/*alert('Hello world from javascript')*/
+/* alert trong js là báo động giúp ta hiển thị thông báo trên trình duyệt 
+    cú pháp alert ('thông tin bản muốn hiển thị')
+*/
